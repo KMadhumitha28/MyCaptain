bec1a87943419303c18945d30ca0318a2edfdf66
KMadhumitha28 <80596373+KMadhumitha28@users.noreply.github.com>
2021-03-15 22:02:39 +0530

    created task2.py
    this program consists 2 sets, From each of the set, the positive numbers are printed

---
+++ task2.py
@@ -0,0 +1,16 @@
+print("hello MyCaptain")
+print("from list1,the following are positive integers")
+List1=[12,-7,5,64,-14]
+for i in List1:
+    if(i>=0):
+        print(i",")
+    else:
+        pass
+print("\n")     
+print("from list2,the following are positive integers")
+List2=[12,14,-95,3]
+for j in List2:
+    if(j>=0):
+        print(j",")
+    else:
+        pass 
