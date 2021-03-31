42726e5cdb618ecb7ed6ebbf14f5ba80f12adcb4
KMadhumitha28 <80596373+KMadhumitha28@users.noreply.github.com>
2021-03-31 22:33:44 +0530

    Created area.py
    Give the value of radius, and it returns the area

---
+++ area.py
@@ -0,0 +1,8 @@
+import math
+
+print("Hello MyCaptain")
+print("In this program we are going to find the area of the circle with a given radius")
+radius=float(input("Enter the measurment of radius in cm: "))
+area=math.pi*radius*radius
+
+print("The area of the circle with {}cm radius is {}cm^2 ".format(radius,area))
